e6a193224e9ed9f210ca1d2031bb7ee1ceae0205
ChickenPige0n <2336983354@qq.com>
2025-11-21 19:55:45 +0800

    chore: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        aileron: ['Aileron', 'ui-sans-serif', 'system-ui'],
+        basiccommerce: ['BasicCommerce', 'Aileron', 'sans-serif']
+      }
+    }
+  }
+}
